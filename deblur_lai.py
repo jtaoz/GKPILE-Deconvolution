4a550afdccf8d946b61e3b61521c88cd5c022aa2
jtaoz <100530159+jtaoz@users.noreply.github.com>
2024-07-15 17:58:26 +0800

    Update deblur_lai.py

--- deblur_lai.py
+++ deblur_lai.py
@@ -120,7 +120,7 @@ for f in files_source:
 
     net_input_saved = net_input.detach().clone()
 
-    save_path = os.path.join(new_path, 'initialization_k1.png')
+    save_path = os.path.join(new_path, 'initialization_k.png')
     out_k_np = torch_to_np(out_k)
     out_k_np = out_k_np.squeeze()
     out_k_np /= np.max(out_k_np)
